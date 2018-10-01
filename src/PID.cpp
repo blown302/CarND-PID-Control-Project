8bd7903010374f577e093f35f0cf74e2c72f902a
Thomas Milas <itpro41994@gmail.com>
2018-09-30 20:41:34 -0700

    initial implementation.

--- src/PID.cpp
+++ src/PID.cpp
@@ -6,16 +6,30 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
+PID::PID() {
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
 
-PID::~PID() {}
+    previous_cte = 0;
+}
+
+PID::~PID() = default;
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+    p_error = cte;
+    i_error += cte;
+    d_error = cte - previous_cte;
+    previous_cte = cte;
 }
 
 double PID::TotalError() {
+    return Kp*p_error + Ki*i_error + Kd*d_error;
 }
 
